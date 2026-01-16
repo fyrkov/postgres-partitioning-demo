8b286a8465e9348fa0ce05bf0605fdeab0e6f1b3
Andrey Chirkov <fyrkov86@gmail.com>
2026-01-16 11:06:25 +0100

    simplify testcontainers setup

--- src/test/kotlin/io/github/fyrkov/postgres_partitioning_demo/AbstractIntegrationTest.kt
+++ src/test/kotlin/io/github/fyrkov/postgres_partitioning_demo/AbstractIntegrationTest.kt
@@ -1,8 +1,7 @@
 package io.github.fyrkov.postgres_partitioning_demo
 
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.testcontainers.postgresql.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 
@@ -13,21 +12,10 @@ abstract class AbstractIntegrationTest {
         private val image = DockerImageName.parse("postgres:17")
 
         @JvmStatic
+        @ServiceConnection
         val postgresContainer: PostgreSQLContainer = PostgreSQLContainer(image)
             .withDatabaseName("postgres")
             .withUsername("postgres")
             .withPassword("secret")
-
-       init {
-            postgresContainer.start()
-        }
-
-        @JvmStatic
-        @DynamicPropertySource
-        fun registerProps(registry: DynamicPropertyRegistry) {
-            registry.add("spring.datasource.url") { postgresContainer.jdbcUrl }
-            registry.add("spring.datasource.username") { postgresContainer.username }
-            registry.add("spring.datasource.password") { postgresContainer.password }
-        }
     }
 }
